97d9f29826d9b8aa81270207fd4cdcade78afd1b
bhojane.v <bhojane.v@internal.media.net>
2022-06-23 16:02:26 +0530

    fixed routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import {Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Store from './components/Store';
 import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Header />
-      <div className='fht'>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path='store' element={<Store />} />
-        </Routes>
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <div className='fht'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='store' element={<Store />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ShopProvider } from './context/ShopContext'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 ReactDOM.render(
   <React.StrictMode>
     <ShopProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='*' element={<App />} />
-        </Routes>
-      </BrowserRouter>
+      <App/>
     </ShopProvider>
   </React.StrictMode>,
   document.getElementById('root') 
